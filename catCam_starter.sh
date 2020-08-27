f2ec18f713cfd30f3069995249cfdd6ca5386aba
niciBume <niciBume@users.noreply.github.com>
2020-08-27 13:05:00 +0200

    Update catCam_starter.sh

--- catCam_starter.sh
+++ catCam_starter.sh
@@ -4,4 +4,4 @@ echo "Executing CatPreyAnalyzer"
 # Tensorflow Stuff
 export PYTHONPATH=$PYTHONPATH:/home/pi/tensorflow/models/research:/home/pi/tensorflow/models/research/slim:/home/pi/.local/lib/python3.7/site-packages
 cd /home/pi/CatPreyAnalyzer
-python3 mega_cascade_doer.py
+python3 cascade.py
