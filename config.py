43dc8db56b331068e9b89a3cb75b92cf891730e4
Bruno Lingner <mail@hugo.ro>
2025-05-16 15:11:30 +0200

    put config variables in config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# Insert Chat ID and Bot Token according to Telegram API
+CHAT_ID = '1234567890'
+BOT_TOKEN = '1234567890:AAEdFbGdrgTDhtEHEThetHE#534HherhETQ'
+
+# Insert webhooks for home assistant
+LOCK_WEBHOOK = "http://192.168.178.24:8123/api/webhook/LockCatFlapNow"
+UNLOCK_WEBHOOK = "http://192.168.178.24:8123/api/webhook/UnlockCatFlapNow"
+
