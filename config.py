53bb93ebee37790c08d4ff3d5916150826797d66
Bruno Lingner <mail@hugo.ro>
2025-05-19 13:50:01 +0200

    store secrets in config file

--- config.py
+++ config.py
@@ -1,10 +1,15 @@
 # config.py
 
 # Insert Chat ID and Bot Token according to Telegram API
-CHAT_ID = '1234567890'
-BOT_TOKEN = '1234567890:AAEdFbGdrgTDhtEHEThetHE#534HherhETQ'
+CHAT_ID = '1234567890' # my Telegram ID
+BOT_TOKEN = '1234567890:SecretTelegramApiToken534HherhETQ' # bot Telegram ID:TOKEN
 
-# Insert webhooks for home assistant
-LOCK_WEBHOOK = "http://192.168.178.24:8123/api/webhook/LockCatFlapNow"
-UNLOCK_WEBHOOK = "http://192.168.178.24:8123/api/webhook/UnlockCatFlapNow"
+# Insert unlock/lock webhooks for home assistant
+#HA_UNLOCK_WEBHOOK = "http://homeassistant.local:8123/api/webhook/-=UnlockCatFlapNow=-"
+#HA_LOCK_OUT_WEBHOOK = "http://homeassistant.local:8123/api/webhook/-=LockOutCatFlapNow=-"
+#HA_LOCK_ALL_WEBHOOK = "http://homeassistant.local:8123/api/webhook/-=LockOutCatFlapNow=-"
+
+# TOKEN from home assistant REST
+#HA_REST_URL = "http://homeassistant.local:8123/api/states/sensor.cat_flap_sureflap"
+#HA_REST_TOKEN = "ChangeThisToYourCreated-SecretHomeAssistantREST-Token"
 
