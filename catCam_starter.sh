a54bcf644f68067c4d06e707e767c789d2d3eabb
Bruno Lingner <mail@hugo.ro>
2025-05-21 22:15:46 +0200

    Empty file can be removed

--- catCam_starter.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Executing CatPreyAnalyzer"
-# Tensorflow Stuff
-export PYTHONPATH=$PYTHONPATH:/home/pi/tensorflow/models/research:/home/pi/tensorflow/models/research/slim:/home/pi/.local/lib/python3.7/site-packages
-cd /home/pi/CatPreyAnalyzer
-python3 cascade.py $@
