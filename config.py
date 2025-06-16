3502caf528e709356cb2fdf9f7bab4150f25169a
netphantm <mail@hugo.ro>
2025-06-16 18:11:12 +0200

    example streams

--- config.py
+++ config.py
@@ -99,9 +99,9 @@ CAM_HEIGHT = 1080
 
 # Optional per-source overrides
 CAMERA_FLIP_OVERRIDES = {
-    "http://192.168.178.22:9000/mjpg": {"hflip": True, "vflip": False},
-    "rtsp://192.168.1.59:8554/unicast": {"hflip": False, "vflip": False},
-    "rtsp://user:pass@192.168.1.58:8554/unicast": {"hflip": False, "vflip": False},
+    "http://192.168.1.22:9000/mjpg": {"hflip": True, "vflip": False},
+    "rtsp://192.168.1.59:554/unicast": {"hflip": False, "vflip": False},
+    "rtsp://XXXX:YYYY@192.168.1.58:8554/unicast": {"hflip": False, "vflip": False},
     "usb:0": {"hflip": False, "vflip": True},  # Simulated USB cam identifier
     "default": {"hflip": False, "vflip": False}
 }
